c087bcf84f5ee72044260bfc5b2041b094a9c8cb
Harrison Court <harrison.court25@gmail.com>
2024-10-06 21:51:05 +1030

    Added System text

--- lua/autorun/chat_strings.lua
+++ lua/autorun/chat_strings.lua
@@ -2,7 +2,7 @@
 
 -- or store them in a table
 myStrings = {
-    serverChat = "[Server]",
+    --serverChat = "",
     healthChat = "[Health]",
-    systemChat = "[System]"
+    systemChat = "<rgb: 191,0,255> [<rgb:210,77,255>System<rgb:191,0,255>] <clr:white>"
 }
