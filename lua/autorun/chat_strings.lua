ea775767905d245a54fcde831332f1faf84f5e11
Harrison Court <harrison.court25@gmail.com>
2024-10-08 22:50:29 +1030

    REnamed shit

--- lua/autorun/chat_strings.lua
+++
@@ -1,8 +0,0 @@
--- lua/autorun/my_strings.lua
-
--- or store them in a table
-myStrings = {
-    --serverChat = "",
-    healthChat = "[Health]",
-    systemChat = "<rgb: 191,0,255> [<rgb:210,77,255>System<rgb:191,0,255>] <clr:white>"
-}
