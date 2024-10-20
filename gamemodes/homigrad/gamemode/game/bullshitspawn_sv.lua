7e733d213f83c39876744bdd45875dde9baebc6c
Deka <112948956+Deka4ka@users.noreply.github.com>
2024-10-21 00:57:56 +0200

    new loot system ( FFA need to be fixed )

--- gamemodes/homigrad/gamemode/game/bullshitspawn_sv.lua
+++ gamemodes/homigrad/gamemode/game/bullshitspawn_sv.lua
@@ -1,3 +1,4 @@
+
 KOROBKA_HUYNYI = {
     "models/props_junk/cardboard_box001a.mdl",
     "models/props_junk/cardboard_box001b.mdl",
@@ -18,8 +19,6 @@ KOROBKA_HUYNYI = {
     "models/props/de_inferno/claypot02.mdl",
     "models/props/de_inferno/claypot01.mdl",
     "models/props_junk/terracotta01.mdl",
-    "models/props_junk/wood_crate002a.mdl",
-    "models/props_junk/wood_crate001a_damagedmax.mdl",
     "models/props_combine/breenbust.mdl",
     "models/props_interiors/Furniture_chair01a.mdl",
     "models/props_c17/FurnitureShelf001a.mdl",
@@ -47,6 +46,7 @@ local weaponsrare = { "weapon_beretta", "weapon_remington870", "weapon_doublebar
 local weaponsveryrare = { "weapon_m3super", "ent_jack_gmod_ezarmor_mtorso", "ent_jack_gmod_ezarmor_mhead" }
 local weaponslegendary = { "weapon_xm1014", "weapon_ar15", "weapon_civil_famas" }
 
+
 local ammos = { "ent_ammo_.44magnum", "ent_ammo_12/70gauge", "ent_ammo_762x39mm", "ent_ammo_556x45mm", "ent_ammo_9х19mm" }
 
 
@@ -69,12 +69,8 @@ end
 hook.Add("PropBreak", "homigrad", function(att, ent)
     if not KOROBKA_HUYNYI_LOOKUP[ent:GetModel()] then return end
 
-    local func = TableRound().ShouldSpawnLoot
-    if not func then return end
-
     local posSpawn = ent:GetPos() + ent:OBBCenter()
     local randomWep, type1 = randomLoot()
-
 
     if randomWep == "*ammo*" then
         if IsValid(att) then
@@ -97,13 +93,82 @@ hook.Add("PropBreak", "homigrad", function(att, ent)
 end)
 
 
+local angZero = Angle(0, 0, 0)
+local vecHull = Vector(0, 0, 0)
+local trCheck = {
+    mask = MASK_SOLID,
+    mins = -vecHull,
+    maxs = vecHull,
+}
+
+local function MakeRandomSpawns(basepoints, iterations, maxiterations, tbl)
+    if iterations >= maxiterations then return tbl end
+    iterations = iterations + 1
+
+    local vecRand = VectorRand(-2048, 2048)
+    vecRand[3] = math.random(8) == 1 and math.abs(vecRand[3]) / math.random(2) or 0
+    local tr = util.QuickTrace(basepoints[math.random(#basepoints)] + vecRand, -vector_up * 256)
+
+    if tr.Hit and not tr.HitSky and not tr.StartedSolid and not (tr.HitTexture == "**studio**" or tr.HitTexture == "**empty**" or tr.HitTexture == "TOOLS/TOOLSNODRAW") then
+        local pos = tr.HitPos + vector_up * 16
+
+        trCheck.start = tr.HitPos
+        trCheck.endpos = tr.HitPos + vector_up * 36
+
+        local tr2 = util.TraceLine(trCheck)
+
+        if not tr2.Hit and not tr2.StartedSolid and not (tr2.HitTexture == "**studio**") then
+            table.insert(basepoints, pos)
+            table.insert(tbl, pos)
+        end
+    end
+    
+    return MakeRandomSpawns(basepoints, iterations, maxiterations, tbl)
+end
+
+
 local spawns = {}
+
 local function cacheSpawns()
     spawns = {}
     for _, ent in ipairs(ents.FindByClass("info_*")) do
         table.insert(spawns, ent:GetPos())
     end
+
+    local navmeshareas = navmesh.GetAllNavAreas()
+    for _, k in pairs(navmeshareas) do
+        table.insert(spawns, k:GetCenter())
+    end
 end
+
+
+hook.Add("InitPostEntity", "CacheSpawnPoints", function()
+    cacheSpawns()
+
+    local tbladd = MakeRandomSpawns(spawns, 0, 500, {})
+    table.Add(spawns, tbladd)
+    
+end)
+
+
+hook.Add("Boxes Think", "SpawnBoxes", function()
+    if #player.GetAll() == 0 or not roundActive or #spawns == 0 then return end  
+    
+    local randomWep = randomLoot()
+    local box = ents.Create(randomWep or "prop_physics")
+
+    if not IsValid(box) then return end
+
+    local spawnPos = spawns[math.random(#spawns)]
+    if spawnPos then
+        box:SetPos(spawnPos + Vector(0, 0, 32))
+        box:Spawn()
+        box.Spawned = true
+    else
+        print("Error: spawn position not found.")
+    end
+end)
+
 
 hook.Add("PostCleanupMap", "addboxs", function()
     cacheSpawns()
@@ -114,17 +179,7 @@ hook.Add("PostCleanupMap", "addboxs", function()
     end)
 end)
 
-hook.Add("Boxes Think", "SpawnBoxes", function()
-    if #player.GetAll() == 0 or not roundActive then return end
-
-    local randomWep = randomLoot()
-    local box = ents.Create(randomWep or "prop_physics")
-
-    if not IsValid(box) then return end
-    box:SetModel(randomWep and "" or table.Random(KOROBKA_HUYNYI)) 
-    box:SetPos(spawns[math.random(#spawns)] + Vector(0, 0, 32))
-    box:Spawn()
-    if randomWep then
-        box.Spawned = true
-    end
+if timer.Exists("SpawnTheBoxes") then timer.Remove("SpawnTheBoxes") end
+timer.Create("SpawnTheBoxes", 15, 0, function()
+    hook.Run("Boxes Think")
 end)
--- gamemodes/homigrad/gamemode/game/levels/ffa/init_sv.lua
+++ gamemodes/homigrad/gamemode/game/levels/ffa/init_sv.lua
@@ -1,86 +1,43 @@
-
 function ffa.StartRoundSV()
     tdm.RemoveItems()
 
     roundTimeStart = CurTime()
-    roundTime = 300 + math.random(0, 300)  
+    roundTime = 300 + math.random(0, 300)
 
     local players = PlayersInGame()
     for i, ply in pairs(players) do
-        ply:SetTeam(1)  
-        ply:SetNWInt("KillCount", 0) 
+        ply:SetTeam(1)
+        ply:SetNWInt("KillCount", 0)
+        ffa.SpawnPlayer(ply) 
     end
 
-    local aviable = ReadDataMap("dm") --; idk how it works
+    local aviable = ReadDataMap("dm")
     aviable = #aviable ~= 0 and aviable or homicide.Spawns()
 
     tdm.SpawnCommand(team.GetPlayers(1), aviable, function(ply)
-        ply:Freeze(true)  
+        ply:Freeze(true)
     end)
 
     freezing = true
-    roundTimeRespawn = CurTime() + 10  
+    roundTimeRespawn = CurTime() + 10
 
     return {roundTimeStart, roundTime}
 end
 
-
-function ffa.RoundEndCheck()
-    local winner = nil
-    local highestKills = 0
-    local topPlayer = nil
-
-    for i, ply in pairs(team.GetPlayers(1)) do
-        local kills = ply:GetNWInt("KillCount", 0)
-
-        if kills >= 50 then
-            winner = ply
-            break
-        end
-
-        if kills > highestKills then
-            highestKills = kills
-            topPlayer = ply
-        end
-    end
-
-    if winner then
-        EndRound(winner)  
-    elseif roundTimeStart + roundTime < CurTime() then
-        EndRound(topPlayer)  
-    end
-end
-
-function ffa.EndRound(winner, topPlayer)
-    if winner then
-        PrintMessage(3, winner:GetName() .. " won with " .. winner:GetNWInt("KillCount") .. " kills!")
-    else
-        PrintMessage(3, "Time is up! No one reached 50 kills.")
-    end
-
-
-    for i, ply in ipairs(player.GetAll()) do
-        ply:SetNWInt("KillCount", 0)
-    end
-end
-
-
-
-function ffa.PlayerSpawn(ply)
-    ply:SetModel(tdm.models[math.random(#tdm.models)])  
-    ply:SetPlayerColor(Vector(0, 1, 0.051))  
+function ffa.SpawnPlayer(ply)
+    ply:SetModel(tdm.models[math.random(#tdm.models)])
+    ply:SetPlayerColor(Vector(0, 1, 0.051))
 
     ply:Give("weapon_hands")
 
-
-    local roundDmType = math.random(1, 4) 
+    local roundDmType = math.random(1, 4)
     if roundDmType == 1 then
         local r = math.random(1, 8)
         ply:Give((r == 1 and "weapon_mp7") or (r == 2 and "weapon_ak74u") or (r == 3 and "weapon_akm") or (r == 4 and "weapon_rpgg" and "weapon_ump") or (r == 5 and "weapon_m4a1") or (r == 6 and "weapon_mk18") or (r == 7 and "weapon_m4a1") or (r == 8 and "weapon_galil"))
         ply:Give("weapon_kabar")
         ply:Give("medkit")
         ply:Give("med_band_big")
-        ply:SetAmmo(90, (r == 1 and 46) or (r == 2 and 44) or (r == 3 and 47) or (r >= 5 and 45))  
+        ply:SetAmmo(90, (r == 1 and 46) or (r == 2 and 44) or (r == 3 and 47) or (r >= 5 and 45))
 
     elseif roundDmType == 2 then
         local r = math.random(1, 4)
@@ -92,7 +49,7 @@ function ffa.PlayerSpawn(ply)
         ply:Give("med_band_big")
         ply:Give("weapon_hg_rgd5")
         ply:SetAmmo(90, (p <= 3 and 49) or (p == 4 and "5.7×28 mm"))
-        ply:SetAmmo(90, 41)  
+        ply:SetAmmo(90, 41)
 
     elseif roundDmType == 3 then
         ply:Give("weapon_deagle")
@@ -115,26 +72,48 @@ function ffa.PlayerSpawn(ply)
     ply:SetLadderClimbSpeed(100)
 end
 
+function ffa.RoundEndCheck()
+    local winner = nil
+    local highestKills = 0
+    local topPlayer = nil
 
+    for i, ply in pairs(team.GetPlayers(1)) do
+        local kills = ply:GetNWInt("KillCount", 0)
 
+        if kills >= 50 then
+            winner = ply
+            break
+        end
 
-
-function ffa.PlayerInitialSpawn(ply)
-    ply:SetTeam(1)
-end
-
-
-function ffa.PlayerCanJoinTeam(ply, teamID)
-    if teamID ~= 1 then
-        ply:ChatPrint("Only one team is available.")
-        return false
+        if kills > highestKills then
+            highestKills = kills
+            topPlayer = ply
+        end
     end
-    return true
+
+    if winner then
+        EndRound(winner)
+    elseif roundTimeStart + roundTime < CurTime() then
+        EndRound(topPlayer)
+    end
 end
 
-damageTracking = {}
+function ffa.EndRound(winner)
+    if winner then
+        PrintMessage(3, winner:GetName() .. " won with " .. winner:GetNWInt("KillCount") .. " kills!")
+    else
+        PrintMessage(3, "Time is up! No one reached 50 kills.")
+    end
 
-hook.Add("EntityTakeDamage", "TrackPlayerDamage", function(target, dmgInfo)
+    for i, ply in ipairs(player.GetAll()) do
+        ply:SetNWInt("KillCount", 0)
+    end
+
+    hook.Remove("EntityTakeDamage", "FFA_TrackPlayerDamage")
+    hook.Remove("PlayerDeath", "FFA_HandlePlayerDeath")
+end
+
+function ffa.TrackPlayerDamage(target, dmgInfo)
     if target:IsPlayer() and dmgInfo:GetAttacker():IsPlayer() then
         local attacker = dmgInfo:GetAttacker()
 
@@ -148,9 +127,9 @@ hook.Add("EntityTakeDamage", "TrackPlayerDamage", function(target, dmgInfo)
 
         damageTracking[target][attacker] = damageTracking[target][attacker] + dmgInfo:GetDamage()
     end
-end)
+end
 
-hook.Add("PlayerDeath", "CustomKillSystem", function(victim, inflictor, attacker)
+function ffa.HandlePlayerDeath(victim)
     if damageTracking[victim] then
         local highestDamage = 0
         local killer = nil
@@ -171,7 +150,21 @@ hook.Add("PlayerDeath", "CustomKillSystem", function(victim, inflictor, attacker
 
     timer.Simple(10, function()
         if IsValid(victim) then
-            victim:Spawn()
+            ffa.SpawnPlayer(victim) 
         end
     end)
-end)
+end
+
+function ffa.PlayerInitialSpawn(ply)
+    ply:SetTeam(1)
+end
+
+function ffa.PlayerCanJoinTeam(ply, teamID)
+    if teamID ~= 1 then
+        ply:ChatPrint("Only one team is available.")
+        return false
+    end
+    return true
+end
+
+damageTracking = {}
