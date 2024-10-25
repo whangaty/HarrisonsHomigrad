e016fdca79ad130597349ad495a4305f062267c9
Harrison Court <harrison.court25@gmail.com>
2024-10-25 17:32:11 +1030

    Fixed potential issue with bodies being too fragile

--- lua/homigrad_scr/game/tier_1/fake/gib/init_sv.lua
+++ lua/homigrad_scr/game/tier_1/fake/gib/init_sv.lua
@@ -256,7 +256,7 @@ hook.Add("EntityTakeDamage","Gib",function(ent,dmgInfo)
 
 	local bonename = ent:GetBoneName(ent:TranslatePhysBoneToBone(phys_bone))
 
-	if ply and bonename == "ValveBiped.Bip01_Head1" then ply:Kill() end
+	--if ply and bonename == "ValveBiped.Bip01_Head1" then ply:Kill() end
 	if ply and not validBone2[bonename] then return end
 
 	if bonetohitgroup[bonename] then hitgroup = bonetohitgroup[bonename] end
