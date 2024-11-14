9e653e41ddf17eb82b3ffe55ba9ecb8ab13e08bf
Harrison Court <harrison.court25@gmail.com>
2024-11-14 18:15:58 +1030

    reaching for the gun

--- gamemodes/homigrad/gamemode/game/level_sh.lua
+++ gamemodes/homigrad/gamemode/game/level_sh.lua
@@ -4,14 +4,8 @@ function TableRound(name) return _G[(name or roundActiveName) or "homicide"] end
 
 timer.Simple(0,function()
     if roundActiveName == nil then --and not (string.find(string.lower(game.GetMap()), "rp_desert_conflict")) then
-        if GetConVar("sv_construct"):GetBool() == true then
-            roundActiveName = "construct"
-            roundActiveNameNext = "construct"
-        else
-            roundActiveName = "homicide"
-            roundActiveNameNext = "homicide"
-        end
-
+        roundActiveName = "homicide"
+        roundActiveNameNext = "homicide"
         StartRound()
     end
 end)
