7f8b7d878217ce7fe067c4450589ca29255c667a
Harrison Court <harrison.court25@gmail.com>
2024-11-14 18:22:58 +1030

    the gun is touching the roof of my mouth

--- gamemodes/homigrad/gamemode/game/level_sh.lua
+++ gamemodes/homigrad/gamemode/game/level_sh.lua
@@ -1,11 +1,17 @@
 LevelList = {}
 
-function TableRound(name) return _G[(name or roundActiveName) or "homicide"] end
+function TableRound(name) return _G[name or roundActiveName] end
 
 timer.Simple(0,function()
     if roundActiveName == nil then --and not (string.find(string.lower(game.GetMap()), "rp_desert_conflict")) then
-        roundActiveName = "homicide"
-        roundActiveNameNext = "homicide"
+        if GetConVar("sv_construct"):GetBool() == true then
+            roundActiveName = "construct"
+            roundActiveNameNext = "construct"
+        else
+            roundActiveName = "homicide"
+            roundActiveNameNext = "homicide"
+        end
+
         StartRound()
     end
 end)
