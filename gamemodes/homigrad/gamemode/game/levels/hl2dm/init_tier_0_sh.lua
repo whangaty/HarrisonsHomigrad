af0f68aa78ce00b2de252c039e1c0d289134b7de
Harrison Court <harrison.court25@gmail.com>
2024-11-21 18:49:17 +1030

    Reverted DM Spawn Changes, Added Snipers as a Traitor Weapon

--- gamemodes/homigrad/gamemode/game/levels/hl2dm/init_tier_0_sh.lua
+++ gamemodes/homigrad/gamemode/game/levels/hl2dm/init_tier_0_sh.lua
@@ -1,4 +1,4 @@
---table.insert(LevelList,"hl2dm")
+table.insert(LevelList,"hl2dm")
 hl2dm = {}
 hl2dm.Name = "Half Life 2 Death Match"
 
@@ -6,7 +6,7 @@ local models = {}
 for i = 1,9 do table.insert(models,"models/player/group03/male_0" .. i .. ".mdl") end
 
 hl2dm.red = {"Resistance",Color(125,95,60),
-	weapons = {"weapon_hands","med_band_big","med_band_small","weapon_radio"},
+	weapons = {"weapon_hands","med_band_big","med_band_small","weapon_radio","weapon_scout"},
 	main_weapon = {"weapon_sar2","weapon_spas12","weapon_akm","weapon_mp7"},
 	secondary_weapon = {"weapon_hk_usp","weapon_p220"},
 	models = models
@@ -15,7 +15,7 @@ hl2dm.red = {"Resistance",Color(125,95,60),
 
 hl2dm.blue = {"Combine",Color(75,75,125),
 	weapons = {"weapon_hands"},
-	main_weapon = {"weapon_sar2","weapon_spas12","weapon_mp7"},
+	main_weapon = {"weapon_sar2","weapon_spas12","weapon_mp7","weapon_scout"},
 	secondary_weapon = {"weapon_hk_usp"},
 	models = {"models/player/combine_soldier.mdl"}
 }
