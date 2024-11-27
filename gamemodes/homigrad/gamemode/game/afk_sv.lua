343965bae14fc30aaeb83a627bb0d9c5b358d160
Harrison Court <harrison.court25@gmail.com>
2024-11-27 23:52:42 +1030

    AFK Adjustments

--- gamemodes/homigrad/gamemode/game/afk_sv.lua
+++ gamemodes/homigrad/gamemode/game/afk_sv.lua
@@ -2,5 +2,5 @@ util.AddNetworkString("afk")
 
 net.Receive("afk",function(len,ply)
 	ply:KillSilent()
-	ply:Kick("Due to High Server Demand, you have been kicked for idling.")
+	ply:Kick("You have been kicked for idling.")
 end)
