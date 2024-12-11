50df237021c6dd14c956a44fd62167cca2b42164
Harrison Court <harrison.court25@gmail.com>
2024-12-11 22:53:59 +1030

    Quick Test Fix

--- gamemodes/homigrad/gamemode/game/levels/homicide/init_tier_0_sh.lua
+++ gamemodes/homigrad/gamemode/game/levels/homicide/init_tier_0_sh.lua
@@ -1,6 +1,6 @@
 table.insert(LevelList,"homicide")
 homicide = homicide or {}
-homicide.Name = language.GetPhrase("rounds.homicide")
+homicide.Name = "Homicide"
 
 homicide.red = {"#team.innocent",Color(255,255,255),
     models = tdm.models
